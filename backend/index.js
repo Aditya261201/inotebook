7cf4c3cd7239bd3bb0257af3d567ebe5878f9a67
adityaKumar <adityakumarrajput01@gmail.com>
2022-12-21 00:54:03 +0530

    3rd commit - Storing Data into the Database Using Mongoose Model

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+// if we want to use req.body then we have to use a middleware ..and here its app.use(express.json())--here by this we can deal in json.
+app.use(express.json())
+
+// Available Routes.
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const User = require('../models/User')
 const router = express.Router();
 
-
-router.get('/', (req, res)=>{
-    obj = {
-        a: 'hello',
-        number: 45
-    }
-    res.json(obj);
+// Create a user using : POST "/api/auth/". Doesn't require auth.
+router.post('/', (req, res)=>{
+    console.log(req.body)
+    const user = User(req.body)   // create a new user that we will give by thunderclient in json form (we will give name,email,password...date is added automatically )
+    user.save()          // save the user in db
+    res.send("done")   // send a response
 })
 
 
