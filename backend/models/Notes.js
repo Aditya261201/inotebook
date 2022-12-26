03400bc0faf69120a3ab127c9bb00381ef00a3b8
adityaKumar <adityakumarrajput01@gmail.com>
2022-12-26 18:49:09 +0530

    9th commit -Fetching all notes & Adding a Note

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+    // here we have to add a user to know whose notes are these.and here it works like a foreign key;
+    // its type is objectid of another schema,and here refrence model is user.
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: 'String',
         required: true
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,53 @@
 const express = require('express');
 const router = express.Router();
+var fetchuser = require('../middleware/fetchuser');
+const Notes = require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
 
-router.get('/', (req, res)=>{
-    res.json([]);
+
+
+//           ROUTE-1                   ------------------------**************************-------------------------
+// Get all the notes using : GET "/api/auth/login" 
+router.get('/fetchallnotes',fetchuser, async (req, res)=>{
+    try{
+        const notes = await Notes.find({user: req.user.id})
+    res.json(notes);
+    }catch(error){
+        console.error(error.message)
+        res.status(500).send("Internal Server Error !! ")
+    }
+})
+
+
+//           ROUTE-2                   ------------------------**************************-------------------------
+// Add a new note using : POST "/api/auth/login" 
+router.post('/addnote',fetchuser,[
+    // validation for notes;
+    body('title','enter a valid title').isLength({min:3}),
+    body('description','description must be at least 5 characters.').isLength({min:5}),
+], async (req, res)=>{
+    try{
+        const {title,description,tag} = req.body; // destructuring notes.
+    // if there are errors then rerturn bad request and the errors 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // making a note and then saving it.
+    const note = new Notes({
+        title,description,tag,user:req.user.id
+    })
+    const savedNote = await note.save();
+
+    res.json(savedNote);
+
+    }catch(error){
+        console.error(error.message)
+        res.status(500).send("Internal Server Error !! ")
+    }
+
 })
 
 
